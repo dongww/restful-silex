445bbaeb761b12a7414ce567cbfebe6ea9348351
dongww <dongww@gmail.com>
2017-03-15 15:46:03 +0800

    移除废弃代码

--- web/index.php
+++ web/index.php
@@ -9,17 +9,4 @@ $app['debug'] = false;
 
 require_once __DIR__ . '/../config/main.php';
 
-//$app->get('/a', function (Application $app) {
-//    $response = $app['http.client']->get('categories/roots');
-//
-//    return $app->json($response->getBody());
-//});
-//
-//$app->post('/', function (Application $app, \Symfony\Component\HttpFoundation\Request $request) {
-////    var_dump($request->get('body'));
-//
-////    return $app->json($request->get('body'));
-//    return $app->notFound();
-//});
-
 $app->run(Request::createFromGlobals());
