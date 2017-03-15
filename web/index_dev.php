55475696abb71cb349f61eda8585bb3ec9505682
dongww <dongww@gmail.com>
2017-03-15 15:21:14 +0800

    新增测试环境前端入口

---
+++ web/index_dev.php
@@ -0,0 +1,25 @@
+<?php
+use Dongww\Rest\Http\Request;
+use SilexRestful\Rest\Application;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Application();
+$app['debug'] = true;
+
+require_once __DIR__ . '/../config/main.php';
+
+//$app->get('/a', function (Application $app) {
+//    $response = $app['http.client']->get('categories/roots');
+//
+//    return $app->json($response->getBody());
+//});
+//
+//$app->post('/', function (Application $app, \Symfony\Component\HttpFoundation\Request $request) {
+////    var_dump($request->get('body'));
+//
+////    return $app->json($request->get('body'));
+//    return $app->notFound();
+//});
+
+$app->run(Request::createFromGlobals());
